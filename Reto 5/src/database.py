7712326f90ef3ba83a3b2ea3723682424a512bb1
Francisco317 <114793808+Francisco317@users.noreply.github.com>
2023-06-07 12:02:07 -0600

    Add files via upload

---
+++ Reto 5/src/database.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host="localhost",
+    user="root", 
+    password="Francisco317",
+    port="3306",
+    database = "python_bdd"
+)
+
+
+
+
+
